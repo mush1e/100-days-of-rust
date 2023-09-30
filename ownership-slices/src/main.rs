cd3765fa89671f6e493dfdd6a9718bf32ce6057e
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-09-29 21:11:41 -0400

    update first_word and add bootiful second_word

--- ownership-slices/src/main.rs
+++ ownership-slices/src/main.rs
@@ -1,12 +1,30 @@
-fn first_word(s :&String) -> usize {
+fn first_word(s :&String) -> &str {
     let bytes = s.as_bytes();
-
-    for(i, &item) in bytes.iter().enumerate() {
+    for (i, &item) in bytes.iter().enumerate() {
         if item==b' ' {
-            return i;
+            return &s[..i];
         }
     }
-    s.len()
+    &s[..]
+}
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    let mut index1 = s.len();
+    let mut index2 = 0;
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            if i < index1 {index1 = i} else {index2 = i; break;};
+        }
+    }
+    if index1 > index2 {return &s[..];}
+    &s[index1..index2]
+}
+
+fn gigachad_slices(s: &String) -> &str {
+    let first_word = &s[..5];
+    first_word
 }
 
 fn main() {
@@ -14,4 +32,8 @@ fn main() {
     let word = first_word(&s);
     println!("last index of the first word is {}", word);
     s.clear();
+
+    let mut s = String::from("My");
+    let word = second_word(&s);
+    println!("{}", word);
 }
