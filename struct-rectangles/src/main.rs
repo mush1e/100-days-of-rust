6015716bec9bd854e700ea8c766a0550a6f502e5
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-09-30 14:42:06 -0400

    Further experimenting with structs and dbg

---
+++ struct-rectangles/src/main.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+fn area (rectangle : &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn main() {
+    let arb_value = 15;
+    let rectangle = Rectangle {
+        width: dbg!(2 * arb_value),
+        height: 50
+    };
+
+    // println!(
+    //     "the area of the rectangle is {} units", area(&rectangle)
+    // );
+
+    dbg!(&rectangle);
+}
