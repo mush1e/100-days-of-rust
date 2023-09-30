56cb54d9639bd655ea06bdb8ffb8fe956d07b526
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-09-30 12:52:13 -0400

    used cool struct update syntax still kinda worried about borrowing here tho

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -17,8 +17,27 @@ fn build_user (email: &str, username: &str) -> User {
 fn main() {
     let username = String::from("Mu5h1e");
     let email = String::from("abc");
-    let mut user1 = build_user(&email, &username);
+    let user1 = build_user(&email, &username);
 
-    user1.email.push_str(" hello there");
     println!("{}", user1.email); 
+
+    // Regular boring ass syntax
+    let mut user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("bar"),
+        sign_in_count: user1.sign_in_count,
+    };
+    user2.active = false;
+    println!("{}", user2.active);
+
+
+    // Cool struct update syntax
+    let cool_user = User {
+        username: String::from("CoolGigaChad"),
+        ..user2
+    };
+
+    println!("{} is an absolute gigachad", cool_user.username);
+    println!("{} is a poopy user", user2.username);
 }
