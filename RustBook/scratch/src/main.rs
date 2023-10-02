822601d0c88cbd94f238a1d31eb57b938d75293b
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-10-01 23:46:24 -0400

    still dont know how to access enum values from within methods

--- RustBook/scratch/src/main.rs
+++ RustBook/scratch/src/main.rs
@@ -10,9 +10,27 @@ impl IpAddrType {
     }
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move {x: i32, y:i32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message {
+    fn call(&self) {
+        // println!("{}", self.Write);
+    }
+}
+
 fn main() {
     let v4_addr = IpAddrType::V4(127, 0, 0, 1);
     let v6_addr = IpAddrType::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("Poggy woggy"));
+    m.call();
+
     println!("{:#?}", v4_addr.call());
 }
 
