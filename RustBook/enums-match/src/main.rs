0df4fe7bf6b43b040c685b9012433dd07d4660e8
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-10-02 14:17:41 -0400

    match and if let v cool

--- RustBook/enums-match/src/main.rs
+++ RustBook/enums-match/src/main.rs
@@ -28,9 +28,29 @@ impl Coin {
     }
 }
 
+fn add_one (x : Option <i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1)
+    }
+}
+
+fn add_two (x : Option<i32>) -> Option<i32>{
+    if let Some(i) = x {
+        println!("{}", i+2);
+        return Some(i+2);
+    }
+    None
+}
+
 fn main() {
     let coin_a = Coin::Penny;
     let coin_b = Coin::Quarter(US_state::Alabama);
     println!("The value of coinA is {} cent", coin_a.value_in_cents());
     println!("CoinB - {} cents", coin_b.value_in_cents());
+
+    let five = Some(5);
+    let six = add_one(five);
+    let seven = add_two(five);
+    let none = add_one(None);
 }
