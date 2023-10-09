3804714d5d63efd03030ddaea8d37960d3f663eb
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-10-09 11:53:21 -0400

    Revisited structs

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,18 +1,3 @@
-#[derive(Debug)]
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-
 fn main() {
-    let mut user_1 = User {
-        username: String::from("Mush1e"),
-        email: String::from("Lul"),
-        sign_in_count: 0,
-        active: true,
-    };
-    user_1.username = String::from("Mu5h1e");
-    println!("{:?}", user_1);
+    println!("Hello world :D");
 }
