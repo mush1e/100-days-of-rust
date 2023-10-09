6140bb3dbfa5a526ec2f9127ed79626c2eaef757
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-10-09 14:35:43 -0400

    strings and iterating over graphemeClusters

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,14 +1,26 @@
+use unicode_segmentation::UnicodeSegmentation;
 fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    v.push(1);
-    v.push(2);
-    v.push(3);
+    // VECTORS
+    // let mut v: Vec<i32> = Vec::new();
+    // v.push(1);
+    // v.push(2);
+    // v.push(3);
 
-    let v2 = vec![1, 2, 3];
-    let v3 = v2.get(20);
-    println!("{:?}", v3);
-    match v.get(20) {
-        Some(i) => println!("{}", i),
-        _ => println!("None"),
-    };
+    // let v2 = vec![1, 2, 3];
+    // let v3 = v2.get(20);
+    // println!("{:?}", v3);
+    // match v.get(20) {
+    //     Some(i) => println!("{}", i),
+    //     _ => println!("None"),
+    // };
+
+    // Strings
+    let s1 = String::new();
+    let s2 = "hello world"; // String slice
+    let s3 = s2.to_string();
+    let s4 = String::from(s2);
+
+    for i in s3.graphemes(true) {
+        println!("{i}");
+    }
 }
