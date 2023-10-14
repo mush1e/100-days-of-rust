4891979c47a423cd117e6f910327fa3a0ad93c5c
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-10-14 11:16:25 -0400

    iterative alternative to recursive solution

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -13,7 +13,20 @@ fn isPrime(num: i32, rec: i32) -> bool {
     ret
 }
 
+fn isPrimeIter(num: i32) -> bool {
+    let mut i = num / 2;
+    while (i > 1) {
+        if num % i == 0 {
+            return false;
+        }
+        i -= 1;
+    }
+    return true;
+}
+
 fn main() {
     println!("64 -> {}", isPrime(64, 63));
     println!("73 -> {}", isPrime(73, 72));
+    println!("64 -> {}", isPrimeIter(64));
+    println!("73 -> {}", isPrimeIter(73));
 }
