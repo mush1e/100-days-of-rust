9a69311391709c9dc9fa3f6786b4b1d854f0b8cb
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-10-14 10:38:53 -0400

    Basic recursion isPrime

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,6 +1,19 @@
 use std::fs::File;
 
+fn isPrime(num: i32, rec: i32) -> bool {
+    let mut ret = false;
+    if num == 1 || rec == 1 {
+        return true;
+    } else if num % rec == 0 {
+        return false;
+    } else {
+        ret = isPrime(num, rec - 1);
+    }
+
+    ret
+}
+
 fn main() {
-    let file = File::open("hello.txt");
-    println!("{:?}", file);
+    println!("64 -> {}", isPrime(64, 63));
+    println!("73 -> {}", isPrime(73, 72));
 }
